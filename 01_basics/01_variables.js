bc6a0774bdd6fe2cca19f8f27d768b2b7890495a
viveksolanki86 <viveksolanki0345@gmail.com>
2024-07-10 17:15:20 +0000

    Staged Changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 13334
+let accountEmail = "viveksolanki@54fg"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState
+
+// accountId = 2
+console.log(accountId);
+accountEmail="dfd@fsdf"
+accountPassword="443"
+accountCity="bangluru"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*preferd  not to use var 
+becouse of issue in block scope and function scope */
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,29 @@
+"use strict"; //treat all js code as  newer version
+
+// alert (3+3) //we are using node js ,not browser
+
+console.log(3+3);
+
+let name="vivek"
+let age="22"
+let isLoggedIn="false"
+
+// Number => 2 to power 52
+// BigInt=>
+// String => ""
+// Boolean=> true /false
+// null => standalone vlue
+// undefined=>
+// Symbol=> 
+    
+    
+// Object 
+
+console.log(typeof 'vivek');
+
+
+
+
+
+
+
